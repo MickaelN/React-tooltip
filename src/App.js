6dee4317f372ac600dacda96a108997c5f22f318
Mickael Noel <mickael.noel@novei.fr>
2022-02-18 11:26:06 +0100

    Ajout d'un formulaire de test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import Tooltip from "./Component/Tooltip"
 function App() {
   return (
     <div className="App">
-      <Tooltip/>
+      <form>
+        <div style={{ width: "100%" }}>
+          <label for="lastname">Nom : </label>
+          <Tooltip>
+            <input type="text" name="lastname" id="lastname"  />
+          </Tooltip>
+        </div>
+        <div style={{ width: "100%" }}>
+          <label for="password">Mot de passe : </label>
+          <Tooltip >
+            <input type="password" name="password" id="password"  />
+          </Tooltip>
+        </div>
+        <div style={{ width: "100%" }}>
+          <Tooltip >
+            <a href="#" >Mot de passe oublié ?</a>
+          </Tooltip>
+        </div>
+      </form>
     </div>
   )
 }
