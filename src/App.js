a3cc36891e3aad6060202d8aadf58bbae1f0e7f6
Mickael Noel <mickael.noel@novei.fr>
2022-02-18 13:08:30 +0100

    Correction d'un bug + ajout de html dans le tooltip

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
       <Header />
       <form>
         <div style={{ width: "100%" }}>
-          <label for="mail">Adresse email : </label>
+          <label htmlFor="mail">Adresse email : </label>
           <Tooltip content="L'adresse email fournit lors de l'inscription" visible={["click"]} hidden={["blur"]}>
             <input type="email" name="mail" id="mail" />
           </Tooltip>
         </div>
         <div style={{ width: "100%" }}>
-          <label for="password">Mot de passe : </label>
-          <Tooltip  content="Le mot de passe de votre compte" visible={["click"]} hidden={["blur"]}>
+          <label htmlFor="password">Mot de passe : </label>
+          <Tooltip  content="<p>Le <strong>mot de passe</strong> de votre compte</p>" visible={["click"]} hidden={["blur"]}>
             <input type="password" name="password" id="password" />
           </Tooltip>
         </div>
--- src/Component/Tooltip/index.js
+++ src/Component/Tooltip/index.js
@@ -1,29 +1,32 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./index.css"
+import parse from "html-react-parser"
 
 const Tooltip = ({ children, content, visible, hidden }) => {
     const [isVisible, setIsVisible] = useState(false)
     const position = "top"
     const className = `tooltiptext ${position}` + (isVisible ? " visible" : " hidden")
-    if (typeof visible === "object") {
-        visible.map(item => {
-            document.getElementById(children.props.id).addEventListener(item, () => {
-                setIsVisible(true)
+    useEffect(() => {
+        if (typeof visible === "object") {
+            visible.map(item => {
+                document.getElementById(children.props.id).addEventListener(item, () => {
+                    setIsVisible(true)
+                })
             })
-        })
-    }
-    if (typeof hidden === "object") {
-        hidden.map(item => {
-            document.getElementById(children.props.id).addEventListener(item, () => {
-                setIsVisible(false)
+        }
+        if (typeof hidden === "object") {
+            hidden.map(item => {
+                document.getElementById(children.props.id).addEventListener(item, () => {
+                    setIsVisible(false)
+                })
             })
-        })
-    }
+        }
+    }, [])
     return (
         <React.Fragment>
             <div className="tooltip">{children}
                 <div className={className} >
-                    {content}
+                    {parse(content)}
                 </div>
             </div>
         </React.Fragment>
