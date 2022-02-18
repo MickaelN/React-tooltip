2b0a6f57273e60b1b3599601478f08eaef0fc059
Mickael Noel <mickael.noel@novei.fr>
2022-02-18 11:27:51 +0100

    Récupération de l'element html dans le composant tooltip

--- src/Component/Tooltip/index.js
+++ src/Component/Tooltip/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import "./index.css"
 
-const Tooltip = () => {
+const Tooltip = ({ children }) => {
     return (
+        <React.Fragment>
+            {children}
             <p>Composant tooltip</p>
+        </React.Fragment>
     );
 }
 export default Tooltip
